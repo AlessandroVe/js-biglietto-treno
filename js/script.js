4778025accbb150d898ef1e154e8c69a16bbcb59
AlessandroVe <aveneziano536@gmail.com>
2021-05-31 10:29:47 +0200

    .

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ if(user === "Ottavio" || user === "Davide" || user === "Alfredo"){
 
 
 var priceKm = 0.21
-var discount18=20
-var discount65=65
+var discount18=.2
+var discount65=.65
 var travel =parseInt(prompt("Quanti km vuoi percorrere?"));
 var ageUser =parseInt(prompt("L'eta' del passeggero?"));
 ageUser=parseInt(ageUser);
 var costTravel = travel * priceKm;
 if (ageUser <= 18){
-    costTravel=(costTravel / 100)*discount18;
+    costTravel=costTravel * discount18;
 }else if(ageUser >= 65) {
-    costTravel=(costTravel / 100)*discount65;
+    costTravel=costTravel * discount65;
 }
 costTravel=costTravel.toFixed(2);
 outputElement.innerHTML+="<br> IL biglietto del treno costera': " + costTravel + "$";
